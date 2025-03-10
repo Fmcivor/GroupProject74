b786c6c5384e42b6a565e0f4bc1fa3698a1f0229
Fmcivor <mcivorfintan@gmail.com>
2025-03-10 10:30:40 +0000

    game page template
    * Basic design for game page
    
    * Pop out bar functionality complete
    
    * Initial commit
    
    * Pop out bar functionality complete (#2)
    
    Co-authored-by: Fmcivor <mcivorfintan@gmail.com>
    
    * dispaly flex to right column in meida query
    
    * minor css changes
    
    * mobile toolbar layout
    
    * New sidebar slides in and does not expand with notebook and inventory layout
    
    * Merge so we both have latest sidebar
    
    * Pull up tool bar transition fixed
    
    * New 2 column layout design
    
    * New 2 column layout merge (#9)
    
    * New sidebar slides in and does not expand with notebook and inventory layout
    
    * New 2 column layout design
    
    * Icons now stay fully visible at all screen sizes
    
    * Left Column formatting
    
    * new button generation
    
    * merging for development of 2 column layout formatting and icon placements (#10)
    
    * New sidebar slides in and does not expand with notebook and inventory layout
    
    * New 2 column layout design
    
    * Icons now stay fully visible at all screen sizes
    
    * new button generation
    
    * pop out tab colour change and added css for the minigame container
    
    * left column sizing and mobile design
    
    * push to main as main template
    
    ---------
    
    Co-authored-by: cgilpin150 <cgilpin150@gmail.com>

---
+++ JAVASCRIPT/OutsideHouse.js
@@ -0,0 +1,152 @@
+//Lead Developer = FINTAN MCIVOR
+//The general layout for all game pages
+// but the game logic such as the button functions and the minigames is coded by each of the lead devleoper for that page
+// we all worked and discussed the best way in which to impleemtn interaction
+// tooll bar -Fintan McIvor
+
+//VARIABLES
+let selectedToolBarItem = null;
+let hasKey = true;
+let electricityOn = false;
+let doorUnlocked = false;
+let clue1 = false;
+
+
+
+//CONSTANTS
+const toolbar = document.querySelector('.toolBar');
+const inventoryButton = document.getElementById('inventoryButton');
+const noteBookButton = document.getElementById('noteBookButton');
+const hideToolBarButton = document.getElementById('hideToolBarButton');
+const noteBookContainer = document.getElementById('noteBook');
+const inventoryContainer = document.getElementById('inventory');
+const promptDivider = document.getElementById('promptDivider')
+
+//EVENT LISTENERS
+inventoryButton.addEventListener('click', showInventory);
+noteBookButton.addEventListener('click', showNoteBook);
+hideToolBarButton.addEventListener('click', hideToolBar);
+
+
+//Show pop out toolbar functions
+function showInventory() {
+
+    noteBookContainer.classList.remove('displayNoteBook');
+    inventoryContainer.classList.add('displayInventory');
+    if (selectedToolBarItem === null) {
+        toolbar.classList.add('toolBarExpanded');
+        hideToolBarButton.classList.add('visible');
+    }
+    selectedToolBarItem = 'inventory';
+
+}
+
+function showNoteBook() {
+    
+    noteBookContainer.classList.add('displayNoteBook');
+    inventoryContainer.classList.remove('displayInventory');
+    if (selectedToolBarItem === null) {
+        toolbar.classList.add('toolBarExpanded');
+        hideToolBarButton.classList.add('visible');
+    }
+
+    selectedToolBarItem = 'noteBook';
+}
+
+function hideToolBar() {
+    selectedToolBarItem = null;
+    hideToolBarButton.classList.remove('visible');
+    toolbar.classList.remove('toolBarExpanded');
+
+    setTimeout(() => {
+        noteBookContainer.classList.remove('displayNoteBook');
+        inventoryContainer.classList.remove('displayInventory');
+    }, 1000);
+
+}
+
+
+//removes transition properties to prevent transitions applying during resizing
+window.addEventListener('resize', function () {
+    //disable transitions
+    toolbar.classList.add('noTransition');
+    inventoryContainer.classList.add('noTransition');
+    noteBookContainer.classList.add('noTransition');
+
+
+    //enable transition
+    setTimeout(() => {
+        toolbar.classList.remove('noTransition');
+        inventoryContainer.classList.remove('noTransition');
+        noteBookContainer.classList.remove('noTransition');
+    }, 1000);
+});
+
+
+//Game interaction - front of house - side of house - shed 
+
+// example layout
+let frontOfHouseDoorLocked = {
+    "room": "Front of House",
+    "description": "You stand infront of a large house with a locked door infront of you and a path leading to your left",
+    "interactions": [
+        {
+            "id": 0,
+            "Text": "Unlock door",
+            "response": unlockDoor
+        },
+        {
+            "id": 1,
+            "Text": "Follow the path to your left",
+            "response": goToSideOfHouse
+        }
+    ]
+}
+let currentState = frontOfHouseDoorLocked;
+let responseId = null;
+
+function unlockDoor(){}
+function goToSideOfHouse(){}
+
+
+
+
+function updateState() {
+    const roomHeader = document.getElementById('roomHeader');
+    const mobileHeader = document.getElementById('mobileHeader')
+    const description = document.getElementById('descriptionParagraph');
+    const responseParagraph = document.getElementById('responseParagraph').textContent = '';
+    const buttonContainer = document.getElementById('buttonContainer');
+    buttonContainer.innerHTML = '';
+    roomHeader.textContent = currentState.room;
+    mobileHeader.textContent = currentState.room;
+    description.textContent = currentState.description;
+
+    
+
+    currentState.interactions.forEach(interaction => {
+
+       let button = document.createElement('button');
+       button.classList.add('optionButton');
+       button.id = interaction.id;
+       button.innerHTML = `<i id="${interaction.id}" class="fa-solid fa-caret-right"></i>&nbsp <p id="${interaction.id}">${interaction.Text}</p>`;
+       button.addEventListener('click', buttonHandler);
+       buttonContainer.appendChild(button);
+    });
+    
+}
+
+updateState();
+
+function buttonHandler(event) {
+    responseId = event.target.id;
+
+    console.log(responseId)
+    if (typeof currentState.interactions[responseId].response === 'string') {
+
+    }
+    else {
+        currentState.interactions[responseId].response();
+    }
+}
+
