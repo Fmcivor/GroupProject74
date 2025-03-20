c2e8a2a2d3effc55f0b0ddbb4b75ced7b6172e0b
matthewcon21 <matthewcon21@gmail.com>
2025-03-20 22:47:49 +0000

    reg added

---
+++ JAVASCRIPT/registration.js
@@ -0,0 +1,204 @@
+
+
+
+const showButton = document.getElementById('togglePassword');
+showButton.addEventListener('click', togglePassword);
+let errorMessage = '<ul>';
+
+function togglePassword() {
+   const password = document.getElementById("password");
+   const confirmPassword = document.getElementById("confirmPassword");
+   if (password.type == "password") {
+      password.type = "text";
+      confirmPassword.type = "text";
+   } else {
+      password.type = "password";
+      confirmPassword.type = "password";
+   }
+}
+
+document.getElementById("registerBtn").addEventListener("click", async function (event) {
+
+   event.preventDefault();
+   errorMessage = "<ul>";
+
+   let username = document.getElementById("username").value;
+   let displayName = document.getElementById('displayName').value;
+   let password = document.getElementById("password").value;
+   let confirmPassword = document.getElementById("confirmPassword").value;
+
+   let validUsername = await validateUsername(username);
+   let validDisplayName = validateDisplayName(displayName);
+   let validPassword = validatePassword(password);
+   let validConfirmPassword = validateConfirmPassword(password, confirmPassword);
+
+   if (validUsername && validDisplayName && validPassword && validConfirmPassword) {
+      console.log("All validations passed. Submitting form...")
+      let insertQuery = `INSERT INTO tblUser (username, userPassword, displayName,iconHREF)
+   VALUES ('${username}', '${password}','${displayName}','placeholder')`;
+      dbConfig.set('query', insertQuery);
+      try {
+         let insertResponse = await fetch(dbConnectorUrl, {
+            method: "POST",
+            body: dbConfig
+         });
+
+         let insertResult = await insertResponse.json();
+
+         if (insertResult.success) {
+            
+            alert("success");
+
+
+            window.location.href = "login.html";
+
+
+         } else {
+            errorMessage = '<p>Error registering user</p>';
+            document.getElementById('messageContent').innerHTML = errorMessage;
+            const headerElement = document.getElementById('messageHeader');
+            headerElement.textContent = "ERROR";
+
+            messageContainer.style.display = 'flex';
+         }
+      } catch (error) {
+         errorMessage = '<p>Error registering user</p>';
+         document.getElementById('messageContent').innerHTML = errorMessage;
+         const headerElement = document.getElementById('messageHeader');
+         headerElement.textContent = "ERROR";
+
+         messageContainer.style.display = 'flex';
+      }
+   }
+   else {
+      errorMessage += '</ul>';
+      document.getElementById('messageContent').innerHTML = errorMessage;
+      const headerElement = document.getElementById('messageHeader');
+      headerElement.textContent = "INVALID";
+
+      messageContainer.style.display = 'flex';
+   }
+});
+
+
+
+
+async function validateUsername(enteredUsername) {
+   let usernameRegex = /^[a-zA-Z0-9.]{5,20}$/;
+   if (usernameRegex.test(enteredUsername) == false) {
+      return false;
+   }
+
+   let selectQuery = `SELECT username FROM tblUser WHERE username = '${enteredUsername}'`;
+   dbConfig.set('query', selectQuery);
+
+   try {
+      let checkResponse = await fetch(dbConnectorUrl, {
+         method: "POST",
+         body: dbConfig
+      });
+
+
+      let checkResult = await checkResponse.json();
+      //username already exists in the database
+      if (checkResult.success && checkResult.data.length > 0) {
+         errorMessage += `<li>Username already exists.</li>`;
+         return false;
+      }
+
+      // Username is valid and doesn't exist
+      return true;
+
+   } catch (error) {
+      console.error("Error checking for existing accounts:", error);
+      errorMessage = '<p></p>';
+      document.getElementById('messageContent').innerHTML = errorMessage;
+      const headerElement = document.getElementById('messageHeader');
+      headerElement.textContent = "ERROR";
+
+      messageContainer.style.display = 'flex';
+
+      return false;
+   }
+}
+
+
+
+
+
+function validateDisplayName(enteredDisplayName) {
+   let displayNameRegex = /^[a-zA-Z-]{1,15}$/;
+   if (displayNameRegex.test(enteredDisplayName) == false) {
+      return false;
+   }
+   return true;
+}
+
+function validatePassword(enteredPassword) {
+   let passwordRegex = /^(?=.*[!@#$%^&*])(?=.*[0-9])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*]{8,20}$/;
+   
+   if (!enteredPassword) {
+      errorMessage += `<li>Password cannot be empty.</li>`;
+      return false;
+   }
+   if (enteredPassword.length < 8 || enteredPassword.length > 20) {
+      errorMessage += `<li>Password must be between 8 and 20 characters long.</li>`;
+      return false;
+   }
+   if (!/[A-Z]/.test(enteredPassword)) {
+      errorMessage += `<li>Password must contain at least one uppercase letter.</li>`;
+      return false;
+   }
+   if (!/[0-9]/.test(enteredPassword)) {
+      errorMessage += `<li>Password must contain at least one number.</li>`;
+      return false;
+   }
+   if (!/[!@#$%^&*]/.test(enteredPassword)) {
+      errorMessage += `<li>Password must contain at least one special character (!@#$%^&*).</li>`;
+      return false;
+   }
+
+   return true;
+}
+
+function validateConfirmPassword(enteredPassword, enteredConfirmPassword) {
+   if (enteredPassword !== enteredConfirmPassword) {
+      errorMessage += `<li>Passwords are not matching.</li>`
+      return false;
+
+   }
+   return true;
+}
+
+
+function displayMessage() {
+   let header = '';
+   let headerColor = '';
+   let message = '';
+   if (success) {
+      header = 'Success';
+      headerColor = 'white';
+      message += '<p>Your profile has successfully been updated!</p>';
+      document.getElementById('messageContent').innerHTML = message;
+   }
+   else {
+      header = 'Invalid';
+      headerColor = 'red';
+
+      errorMessage += '<ul>';
+
+      document.getElementById('messageContent').innerHTML = errorMessage;
+   }
+
+
+
+
+
+}
+
+const closeBtn = document.querySelector('.closeBtn');
+closeBtn.addEventListener('click', function () {
+   messageContainer.style.display = 'none';
+});
+
+
